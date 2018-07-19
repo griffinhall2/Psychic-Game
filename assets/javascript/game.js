3f6562d4f176b3d35543866d67bf597523992154
griffinhall2 <griffin.hall@yahoo.com>
2018-07-19 18:44:53 -0400

    made wins and losses appear

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,25 +3,50 @@
 
 var win = 0;
 var loss = 0;
-var guessesLeft = 0;
-var guessesSoFar = 0;
+var guessesLeft = 9;
+var guessesSoFar = [];
 
 
 //List out possible characters
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //make computer randomly choose letter
-var randomLetter = letters[Math.floor(Math.random() * letters.length)];
-var computerChoice = randomLetter;
+var computerChoice = letters[Math.floor(Math.random() * letters.length)];
 // log computers letter to console
 console.log(computerChoice);
+
+//reset game to start new one
+function reset() {
+    guessesLeft = 10;
+    guessesSoFar = [];
+}
+
 
 //make user choose letter
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
+    
+    console.log(userGuess);
+
+    if (userGuess === computerChoice){
+        win++;
+        alert("You win! You read my mind!");
+        document.querySelector("#win").innerHTML = "Wins: " + win;
+        reset();
+    }
+    
+    if (userGuess !== computerChoice) {
+        guessesLeft--;
+        guessesSoFar.push(userGuess);
+    }
+
+    if (guessesLeft === 0) {
+        loss++;
+        alert("Sorry, you lose...");
+        document.querySelector("#loss").innerHTML = "Losses: " + loss;
+        reset();
+    }
 }
-
-
 
 
